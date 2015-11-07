a46aa4505222c1caa9d880991dbf4b47dcbf61dc
koffee00 <alvin@Castor.local>
2015-11-07 15:01:33 -0800

    Course Names and Course Links successfully scraped
    URL:
    https://www.futurelearn.com/courses/categories/teaching-and-studying
    19
    Course Name: Blended Learning Essentials: Getting Started
    Course LinkL /courses/blended-learning-getting-started
    
    Course Name: How to Succeed at: Interviews
    Course LinkL /courses/interviews
    
    Course Name: Get Started with Online Learning
    Course LinkL /courses/online-learning
    
    Course Name: A Beginner's Guide to Writing in English for University
    Study
    Course LinkL /courses/english-for-study
    
    Course Name: Preparing for University
    Course LinkL /courses/preparing-for-uni
    
    Course Name: Teaching Literacy Through Film
    Course LinkL /courses/teaching-literacy-through-film
    
    Course Name: Academic Integrity: Values, Skills, Action
    Course LinkL /courses/academic-integrity
    
    Course Name: Blended Learning Essentials: Embedding Practice
    Course LinkL /courses/blended-learning-embedding-practice
    
    Course Name: What is Character? Virtue Ethics in Education
    Course LinkL /courses/what-is-character
    
    Course Name: How to Succeed at: Writing Applications
    Course LinkL /courses/writing-applications
    
    Course Name: Understanding Language: Learning and Teaching
    Course LinkL /courses/understanding-language
    
    Course Name: Logical and Critical Thinking
    Course LinkL /courses/logical-and-critical-thinking
    
    Course Name: Preparing for University
    Course LinkL /courses/preparing-for-uni
    
    Course Name: Developing Your Research Project
    Course LinkL /courses/research-project
    
    Course Name: Assessment for Learning in STEM Teaching
    Course LinkL /courses/assessment-for-learning
    
    Course Name: Dyslexia and Foreign Language Teaching
    Course LinkL /courses/dyslexia
    
    Course Name: Professional Practices for English Language Teaching
    Course LinkL /courses/english-language-teaching
    
    Course Name: Study Skills for International Students
    Course LinkL /courses/study-skills-for-international-students
    
    Course Name: Teaching Computing
    Course LinkL /courses/teaching-computing

--- SiteScraper/src/FutureLearn/FutureLearn.java
+++ SiteScraper/src/FutureLearn/FutureLearn.java
@@ -9,6 +9,20 @@ import java.io.IOException;
 import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 
+/**
+ * FutureLearn.java scrapes the FutureLearn website using JSoup library
+ * Extracted data will be inserted to the moocs database
+ * 
+ * VARIABLES
+ * url1 = the url to add into urlXSS
+ * urlXSS = the arraylist that holds the urls to scrape
+ * courseNames = holds the courseNames
+ * courseLinks = holds the courseLinks
+ * 
+ * @author alvin ko 
+ * @author dave tseng
+ *
+ */
 public class FutureLearn
 {
 	 //JDBC DRIVER NAME AND DATABASE URL
@@ -31,18 +45,18 @@ public class FutureLearn
 	{
 		//URLS
 		String url1 = "https://www.futurelearn.com/courses/categories/teaching-and-studying"; 			//FutureLearn: Teaching & Studying
-		String url2 = "https://www.futurelearn.com/courses/categories/business-and-management"; 		//FutureLearn: Business & Management 
-		String url3 = "https://www.futurelearn.com/courses/categories/creative-arts-and-media"; 		//FutureLearn: Creative Arts & Media
-		String url4 = "https://www.futurelearn.com/courses/categories/health-and-psychology"; 			//FutureLearn: Health and Psychology 
-		String url5 = "https://www.futurelearn.com/courses/categories/history"; 						//FutureLearn: History 
-		String url6 = "https://www.futurelearn.com/courses/categories/languages-and-cultures"; 			//FutureLearn: Languages & Cultures
-		String url7 = "https://www.futurelearn.com/courses/categories/law"; 							//FutureLearn: Law
-		String url8 = "https://www.futurelearn.com/courses/categories/literature"; 						//FutureLearn: Literature
-		String url9 = "https://www.futurelearn.com/courses/categories/nature-and-environment"; 			//FutureLearn: Nature & Environment 
-		String url10 = "https://www.futurelearn.com/courses/categories/online-and-digital";				//FutureLearn: Online & Digital 
-		String url11 = "https://www.futurelearn.com/courses/categories/politics-and-the-modern-world"; 	//FutureLearn: Politics & the Modern World 
-		String url12 = "https://www.futurelearn.com/courses/categories/science-maths-and-technology"; 	//FutureLearn: Science, Maths, & Technology 
-		String url13 = "https://www.futurelearn.com/courses/categories/sport-and-leisure";				//FutureLearn: Sport & Leisure
+//		String url2 = "https://www.futurelearn.com/courses/categories/business-and-management"; 		//FutureLearn: Business & Management 
+//		String url3 = "https://www.futurelearn.com/courses/categories/creative-arts-and-media"; 		//FutureLearn: Creative Arts & Media
+//		String url4 = "https://www.futurelearn.com/courses/categories/health-and-psychology"; 			//FutureLearn: Health and Psychology 
+//		String url5 = "https://www.futurelearn.com/courses/categories/history"; 						//FutureLearn: History 
+//		String url6 = "https://www.futurelearn.com/courses/categories/languages-and-cultures"; 			//FutureLearn: Languages & Cultures
+//		String url7 = "https://www.futurelearn.com/courses/categories/law"; 							//FutureLearn: Law
+//		String url8 = "https://www.futurelearn.com/courses/categories/literature"; 						//FutureLearn: Literature
+//		String url9 = "https://www.futurelearn.com/courses/categories/nature-and-environment"; 			//FutureLearn: Nature & Environment 
+//		String url10 = "https://www.futurelearn.com/courses/categories/online-and-digital";				//FutureLearn: Online & Digital 
+//		String url11 = "https://www.futurelearn.com/courses/categories/politics-and-the-modern-world"; 	//FutureLearn: Politics & the Modern World 
+//		String url12 = "https://www.futurelearn.com/courses/categories/science-maths-and-technology"; 	//FutureLearn: Science, Maths, & Technology 
+//		String url13 = "https://www.futurelearn.com/courses/categories/sport-and-leisure";				//FutureLearn: Sport & Leisure
 		
 		 
 		//Arraylist urlXSS contains the URLS that this program will XSS with JSOUP
@@ -74,14 +88,36 @@ public class FutureLearn
 		{
 			String furl = (String) urlXSS.get(i);  //get website url
 			System.out.println("URL: " + furl); //return the website url
-			Document doc = Jsoup.connect(furl).get(); //get HTML code from website
+			Document doc = Jsoup.connect(furl).get(); //get HTML code from the website
+			//System.out.println("HTML document: " + doc); // return the HTML code from the website
 			
-			//Find the link
+			//Find the courseNames
 			Elements link = doc.select("a[href*=courses]"); //get links with hyperlinks containing /courses/
 			
+			ArrayList courseNames = new ArrayList<String>(); //holds the course names
+			ArrayList courseLinks = new ArrayList<String>(); //holds the course links
+			//System.out.println(link.size());
 			for(int j = 0; j < link.size(); j++)
 			{
-				System.out.println("Link: " + link.get(j).attr("href"));
+				String courseName = link.get(j).text();
+				String courseLink = link.get(j).attr("href");
+				if(courseName.equals("") || courseName.equals("More") || courseName.equals("Courses") || courseName.equals("View all categories"))
+				{
+					//don't add to courseNames
+				}
+				else
+				{
+					courseNames.add(courseName);
+					courseLinks.add(courseLink);
+				}
+			}
+			
+			System.out.println(courseNames.size());
+			for(int a = 0; a < courseNames.size(); a++)
+			{
+				System.out.println("Course Name: " + courseNames.get(a));
+				System.out.println("Course LinkL " + courseLinks.get(a));
+				System.out.println();
 			}
 			
 			/*
@@ -95,6 +131,7 @@ public class FutureLearn
 				}
 			END: TEST
 			*/
-		}
-		}
+			
+		} //end loop
+		}//end main
 }
